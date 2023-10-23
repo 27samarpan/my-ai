1717e419bf9928b49a1c1d0129cb0ce1871f2e60
Samarpan Jain <108479358+27samarpan@users.noreply.github.com>
2023-10-23 10:53:31 +0530

    Add files via upload

---
+++ components/free-counter.tsx
@@ -0,0 +1,50 @@
+"use client"
+import { useEffect, useState } from "react";
+import { Card, CardContent } from "./ui/card";
+import { MAX_FREE_COUNTS } from "@/constants";
+import { Progress } from "./ui/progress";
+import { Button } from "./ui/button";
+import { Zap } from "lucide-react";
+import { usePromodal } from "@/hooks/use-pro-modal";
+
+interface FreeCounterProps{
+    apiLimitCount: number;
+    isPro: boolean;
+};
+
+export const FreeCounter=({
+    apiLimitCount = 0,
+    isPro = false,
+}: FreeCounterProps)=>{
+    const proModal = usePromodal();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(()=>{
+        setMounted(true);
+    }, []);
+
+    if(!mounted){
+        return null;
+    }
+
+    if(isPro){
+        return null;
+    }
+
+    return (
+        <div className="px-3">
+            <Card className="bg-white/10 border-0">
+                <CardContent className="pt-2">
+                    <div className="text-center text-sm text-white mb-2 space-y-2">
+                        <p>{apiLimitCount} / {MAX_FREE_COUNTS} Free Generations</p>
+                        <Progress className="h-2" value={(apiLimitCount/MAX_FREE_COUNTS)*100}/>
+                    </div>
+                    <Button onClick={proModal.onOpen} className="text-sm w-full" variant={"premium"}>
+                        Upgrade
+                        <Zap className="w-4 h-4 ml-2 fill-white"/>
+                    </Button>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
